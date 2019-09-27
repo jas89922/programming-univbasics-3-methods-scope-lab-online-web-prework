3abb9f84162f0daa44562553e2ab3255a1179487
Jasmine Kim <jas89922@gmail.com>
2019-09-27 02:52:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,6 +8,8 @@ def toadstool
 end
 def link
   puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts "It's Dangerous To Go Alone! Take This."
+  puts phrase = "It's Dangerous To Go Alone! Take This."
+end
 def any_phrase(phrase)
   puts phrase = "Do A Barrel Roll!"
+end
